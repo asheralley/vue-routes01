77b553c4aa1589abae88e446d4f8c53b516e9b16
asheralley <asher.asheralley@gmail.com>
2018-08-09 13:15:23 +1200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/firstroute/Asher">FirstRoute</router-link></li>
+        <li><router-link to="/firstroute/Asher/child">FirstRouteChild</router-link></li>
+      </ul>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/FirstRoute.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="firstroute">
+    <h2>{{msg}}</h2>
+    Hello {{ $route.params.name }}
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstRoute',
+  data () {
+    return {
+      msg: 'This is the first route... yay XD'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
